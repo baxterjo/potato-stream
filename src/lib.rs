ec974cc646490a79ce09f1bbc6280231700a28c4
Jordan Baxter <jordan.baxter@ditto.live>
2025-03-07 14:27:46 -0800

    Changed creds

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 pub mod app;
 pub mod join_map;
 pub mod stream;
+pub mod utils;
 
 #[cfg(feature = "media")]
 pub mod media;
 
 use clap::{Parser, Subcommand};
+
+const APP_ID: &str = "316d9de7-20e8-4035-8d55-90e702ba7291";
+const OFFLINE_TEST_TOKEN: &str = "o2d1c2VyX2lkdTEwNjY2MzIwMDM3NDExNDA1MDEyN2ZleHBpcnl4GDIwMjUtMDQtMDZUMDY6NTk6NTkuOTk5WmlzaWduYXR1cmV4WGFkbE5wWVNyL2kxMStET1RRZkRDUXN6bG82dzRjK2UyMWV3SDIrenYvSUduTWNDNGJ3Zm1IR1FreWtRVE1LQlp3QWhJNm0rck5qQ0xsR28wblZqNzlnPT0=";
 
 #[derive(Debug, Parser, Clone)]
 #[command(version, about)]
